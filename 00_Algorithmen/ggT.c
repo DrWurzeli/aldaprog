6943fb3d7a8e1059b6d22065b68777c75f09ecf7
BuildTools <github@maxmailbox.de>
2024-03-01 22:25:22 +0100

    moved input func to header file

--- 00_Algorithmen/ggT.c
+++ 00_Algorithmen/ggT.c
@@ -1,18 +1,5 @@
 #include <stdlib.h>
-#include <stdio.h>
-
-int eingabe(){
-    int input = -1;
-    do{
-        printf("Bitte eine Zahl eingeben: ");
-        if(scanf("%d", &input) != 1){
-            scanf("%*[^/n]");
-            input = -1;
-            continue;
-        }
-    }while(input < 0);
-    return input;
-}
+#include "eingabe.h"
 
 int ggT(int z1, int z2){
     if (z1 == 0){
